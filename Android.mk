7d7caf4090388fb9c277cba8213d913e390f22ff
Dhananjay Kumar <dhakumar@codeaurora.org>
2013-10-07 11:38:46 -0700

    Initial mpq8092 HAL upload
    Depends-on: 531569 531557
    Change-Id: Ic130ab0a5ae2ffee09d98b7ca2c3ee4374965466
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -6,7 +6,11 @@ MY_LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
 include $(MY_LOCAL_PATH)/legacy/Android.mk
 else
+ifneq ($(filter mpq8092,$(TARGET_BOARD_PLATFORM)),)
+include $(MY_LOCAL_PATH)/hal_mpq/Android.mk
+else
 include $(MY_LOCAL_PATH)/hal/Android.mk
+endif
 include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/mm-audio/Android.mk
 include $(MY_LOCAL_PATH)/policy_hal/Android.mk
